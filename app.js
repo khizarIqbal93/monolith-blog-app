b1ac2c7cce8829e4d84fc7f31acdaf1bb0fb7976
Khizar Iqbal <khizar.iqbal@ecs.co.uk>
2021-10-26 15:47:17 +0100

    made changes

--- app.js
+++ app.js
@@ -5,15 +5,41 @@ const app = express();
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 const port = 9090;
+const blogs = [];
+fs.readdir("./articles", "utf-8").then((namesArr) => {
+	namesArr.forEach((x) => {
+		const nameWithoutExt = x.substring(0, x.length - 4);
+		const cleanName = nameWithoutExt.replace(/_/gm, " ");
+		fs.readFile(`./articles/${x}`, "utf-8").then((preview) => {
+			const snippet = preview.substr(0, 94) + "...";
+			blogs.push({
+				title: cleanName.charAt(0).toUpperCase() + cleanName.slice(1),
+				snippet,
+			});
+		});
+	});
+});
+
+app.get("/blogs", (req, res) => {
+	res.render("blogs", { blogs });
+});
+
+app.get("/blogs/1", (req, res) => {
+	const article = {};
+	fs.readFile("./articles/milestones.txt", "utf-8").then((file) => {
+		article.title = "Milestones";
+		article.content = file;
+		res.render("blog", {
+			blog: {
+				title: article.title,
+				content: article.content,
+			},
+		});
+	});
+});
 
 app.get("/", (req, res) => {
 	res.render("home");
-});
-app.get("/articles", (req, res) => {
-	// fs.readFile("./articles/milestones.txt", "utf-8").then((file) => {
-	// 	res.send({ title: "Milestones", article: file });
-	// });
-	res.render("articles");
 });
 
 app.listen(port, () => {
