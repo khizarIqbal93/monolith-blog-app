47736bf38649ba30e1fb837b4c5f1fd9cc39d94b
Khizar Iqbal <khizar.iqbal@ecs.co.uk>
2021-11-23 10:27:01 +0000

    tests fixed

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -5,13 +5,9 @@ describe("preview", () => {
 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	const snippet = preview(text);
 	it("it should be a string", () => {
-		return getBlogContent("IDC.txt", "./articles/").then((data) => {
-			expect(typeof snippet).toBe("string");
-		});
+		expect(typeof snippet).toBe("string");
 	});
-	it("it should have a length of 97", () => {
-		return getBlogContent("IDC.txt", "./articles/").then((data) => {
-			expect(snippet).toHaveLength(125);
-		});
+	it("it should have a length of 128", () => {
+		expect(snippet).toHaveLength(128);
 	});
 });
